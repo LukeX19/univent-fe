fd493913cd280360cb494b633cfbab7328044ebc
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-04-24 23:09:43 +0300

    Created generic Event Page with its components

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/home_page/home_page.js"
 import Feed from "./components/pages/feed/feed.js"
-
+import EventPage from "./components/pages/event_page/event_page.js"
 function App() {
   return (
     <>
@@ -9,6 +9,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage/>}/>
           <Route exact path="/feed" element={<Feed/>}/>
+          <Route exact path="/event-page" element={<EventPage/>}/>
         </Routes>
       </BrowserRouter>
     </>
