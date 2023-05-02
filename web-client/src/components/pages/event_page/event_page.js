f5285ebc0873e06986c10e2ee0e6e0c74e44bb12
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-05-02 20:54:25 +0300

    Added array for participants list and refactored code

--- web-client/src/components/pages/event_page/event_page.js
+++ web-client/src/components/pages/event_page/event_page.js
@@ -1,16 +1,16 @@
 import { React, useState } from 'react';
 import NavbarLoggedIn from "../../navbar_logged/navbar_logged.js";
-import { Box, Button, Grid, Slide, Paper, Typography, Dialog, DialogActions, DialogContent, DialogTitle, Autocomplete, TextField, Divider, Avatar, Rating } from '@mui/material';
-import cooking from "../../images/cooking.png";
-import "../event_page/event_page.css"
+import { Box, Button, Grid, Slide, Paper, Typography, Dialog, DialogContent, DialogTitle, Divider, Avatar, Rating } from '@mui/material';
 import Diversity3Icon from '@mui/icons-material/Diversity3';
 import PinDropIcon from '@mui/icons-material/PinDrop';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import girl from "../../images/girl.jpg"
-import maps from "../../images/maps.jpg"
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import cooking from "../../images/cooking.png";
+import girl from "../../images/girl.jpg"
+import maps from "../../images/maps.jpg"
+import "../event_page/event_page.css"
 
 const EventPage = () => {
 
@@ -24,6 +24,51 @@ const EventPage = () => {
         setOpen(false);
     }
 
+    const participants = [
+        {   
+            image: '',
+            lastname: "Chiara",
+            firstname: "Charlotte - Ava",
+            rating: 4.5
+        }, 
+        {   
+            image: '',
+            lastname: "Chiara",
+            firstname: "Charlotte - Ava",
+            rating: 4.5
+        }, 
+        {   
+            image: '',
+            lastname: "Chiara",
+            firstname: "Charlotte - Ava",
+            rating: 4.5
+        }, 
+        {   
+            image: '',
+            lastname: "Chiara",
+            firstname: "Charlotte - Ava",
+            rating: 2.5
+        }, 
+        {   
+            image: '',
+            lastname: "Chiara",
+            firstname: "Charlotte - Ava",
+            rating: 4.5
+        }, 
+        {   
+            image: '',
+            lastname: "Chiara",
+            firstname: "Charlotte - Ava",
+            rating: 1.5
+        }, 
+        {   
+            image: '',
+            lastname: "Chiara",
+            firstname: "Charlotte - Ava",
+            rating: 4.5
+        }, 
+    ];
+
     return(
         <>
             <NavbarLoggedIn/>
@@ -33,13 +78,13 @@ const EventPage = () => {
                     <Grid item className="grid-image">
                         <img src={cooking} alt=""/>
                         <Box className="box">
-                            <Typography sx={{pl: 2, py: 1, color: 'white', fontSize: '20px'}}>Cooking</Typography>
+                            <Typography pl={2} py={1} className="placeholder">Cooking</Typography>
                         </Box>
                     </Grid>
 
                     <Grid container my={2}>
                         <Grid item xs={12} md={8} py={2}>
-                            <Typography sx={{fontSize: '30px', py: 1}}>"Taste - Tastic" I Can Cook</Typography>
+                            <Typography py={1} fontSize="30px">"Taste - Tastic" I Can Cook</Typography>
                             <Grid container>
                                 <Grid item xs={12} py={1}>
                                     <Typography>Created by</Typography>
@@ -57,13 +102,13 @@ const EventPage = () => {
                         <Grid item py={2} xs={12} md={4}>
                             <Grid container>
                                 {[
-                                    { icon: <Diversity3Icon sx={{fontSize: '1.5rem'}} />, text: '3/6 Participants Joined' },
-                                    { icon: <PinDropIcon sx={{fontSize: '1.5rem'}} />, text: 'Victory Square, Timisoara' },
-                                    { icon: <CalendarMonthIcon sx={{fontSize: '1.5rem'}} />, text: '28 Oct. 2023' },
-                                    { icon: <AccessTimeIcon sx={{fontSize: '1.5rem'}} />, text: '18:00' },
+                                    {icon: <Diversity3Icon className="icon"/>, text: "3/6 Participants Joined"},
+                                    {icon: <PinDropIcon className="icon"/>, text: "Victory Square, Timisoara"},
+                                    {icon: <CalendarMonthIcon className="icon"/>, text: "28 Oct. 2023"},
+                                    {icon: <AccessTimeIcon className="icon"/>, text: "18:00"},
                                 ].map(({ icon, text }, index) => (
                                     <Grid item key={index} xs={12} py={1} display="flex">
-                                        {icon} <Typography sx={{fontSize: '18px', px: 3}}>{text}</Typography>
+                                        {icon} <Typography px={3} fontSize="18px">{text}</Typography>
                                     </Grid>
                                 ))}
                             </Grid>
@@ -91,56 +136,33 @@ const EventPage = () => {
                         </Box>
                     </Grid> */}
                     <Grid item xs={12}>
-                        <Box display='flex' justifyContent='space-between'>
-                            <Button variant="contained" onClick={handleOpen} sx={{ background: '#8FBDD3'}}>CHECK ENROLLED PARTICIPANTS</Button>
+                        <Box display="flex" justifyContent="flex-end">
+                            <Button variant="contained" onClick={handleOpen} sx={{mr: 2, background: "#8FBDD3"}}>CHECK ENROLLED PARTICIPANTS</Button>
                             <Button variant="contained">JOIN EVENT</Button>
                         </Box>
                     </Grid>
                 </Paper>
             </Grid>
-            <Dialog className="dialog" scroll={"paper"} onClose={handleClose} open={open} TransitionComponent={Slide} TransitionProps={{direction: 'up', timeout: {enter: 600, exit: 300}}}>
-                <DialogTitle sx={{ textAlign: 'center' }}>List of Participants
-                    <IconButton onClick={handleClose} sx={{position: 'absolute', right: 9, top: 10}}>
+            <Dialog className="dialog-event-page" scroll={"paper"} onClose={handleClose} open={open} TransitionComponent={Slide} TransitionProps={{direction: "up", timeout: {enter: 600, exit: 300}}}>
+                <DialogTitle sx={{textAlign: 'center'}}>List of Enrolled Participants
+                    <IconButton onClick={handleClose} sx={{position: "absolute", right: 9, top: 10}}>
                         <CloseIcon />
                     </IconButton>
                 </DialogTitle>
                 <DialogContent className="dialog-content" dividers={true}>
-                        <Grid container py={2} sx={{justifyContent: 'center', alignItems: 'center'}}>
-                            <Grid item mr={2}>
-                                <Avatar alt="avatar" src={girl}>CC</Avatar>
+                    {participants.map((participant, index) => {
+                        return(
+                            <Grid container py={2} className="participant-container" key={index}>
+                                <Grid item mr={1.5}>
+                                    <Avatar alt="avatar" src={participant.image}>{participant.lastname.charAt(0)}{participant.firstname.charAt(0)}</Avatar>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>{participant.lastname} {participant.firstname}</Typography>
+                                    <Rating readOnly precision={0.5} value={participant.rating}/>
+                                </Grid>
                             </Grid>
-                            <Grid item>
-                                <Typography>Chiara Charllote - Ava</Typography>
-                                <Rating readOnly precision={0.5} value={4.5}/>
-                            </Grid>
-                        </Grid>
-                        <Grid container py={2} sx={{justifyContent: 'center', alignItems: 'center'}}>
-                            <Grid item mr={2}>
-                                <Avatar alt="avatar" src={girl}>CC</Avatar>
-                            </Grid>
-                            <Grid item>
-                                <Typography>Chiara Charllote - Ava</Typography>
-                                <Rating readOnly precision={0.5} value={4.5}/>
-                            </Grid>
-                        </Grid>
-                        <Grid container py={2} sx={{justifyContent: 'center', alignItems: 'center'}}>
-                            <Grid item mr={2}>
-                                <Avatar alt="avatar" src={girl}>CC</Avatar>
-                            </Grid>
-                            <Grid item>
-                                <Typography>Chiara Charllote - Ava</Typography>
-                                <Rating readOnly precision={0.5} value={4.5}/>
-                            </Grid>
-                        </Grid>
-                        <Grid container py={2} sx={{justifyContent: 'center', alignItems: 'center'}}>
-                            <Grid item mr={2}>
-                                <Avatar alt="avatar" src={girl}>CC</Avatar>
-                            </Grid>
-                            <Grid item>
-                                <Typography>Chiara Charllote - Ava</Typography>
-                                <Rating readOnly precision={0.5} value={4.5}/>
-                            </Grid>
-                        </Grid>
+                        )
+                    })}
                 </DialogContent>
             </Dialog> 
         </>
