18019bec10fb10314de47393bf8fb903ed7245e1
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-05-02 23:08:02 +0300

    Added Sign In page

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -4,6 +4,7 @@ import Feed from "./components/pages/feed/feed.js"
 import EventPage from "./components/pages/event_page/event_page.js"
 import CreateEvent from "./components/pages/create_event/create_event.js"
 import ChangePassword from "./components/pages/change_password/change_password.js";
+import Signin from "./components/pages/signin/signin.js";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/event-page" element={<EventPage/>}/>
           <Route exact path="/create-event" element={<CreateEvent/>}/>
           <Route exact path="/change-password" element={<ChangePassword/>}/>
+          <Route exact path="/signin" element={<Signin/>}/>
         </Routes>
       </BrowserRouter>
     </>
