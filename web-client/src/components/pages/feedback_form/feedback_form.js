0782145442fce7bae9c07f5bd9a96361efcfe5d8
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-05-22 12:44:41 +0300

    Connected Feedback Form to Axios requests

--- web-client/src/components/pages/feedback_form/feedback_form.js
+++ web-client/src/components/pages/feedback_form/feedback_form.js
@@ -1,49 +1,69 @@
-import { React } from 'react';
-import { Box, Button, Grid, Paper, Typography, Avatar, Rating } from '@mui/material';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import NavbarLoggedIn from '../../navbar_logged/navbar_logged.js';
-import '../feedback_form/feedback_form.css';
+import { React, useState, useEffect } from "react";
+import { Box, Button, Grid, Paper, Typography, Avatar, Rating } from "@mui/material";
+import { useParams } from "react-router-dom";
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+import EventBusyIcon from '@mui/icons-material/EventBusy';
+import NavbarLoggedIn from "../../navbar_logged/navbar_logged.js";
+import { getEventById, getParticipantsByEventId, addRating } from "../../../api/index.js";
+import { format } from "date-fns";
+import { deepOrange } from "@mui/material/colors";
+import "../feedback_form/feedback_form.css";
 
 const FeedbackForm = () => {
+    const param = useParams();
 
-    const participants = [
-        {   
-            image: '',
-            lastname: "Chiara",
-            firstname: "Charlotte - Ava",
-        }, 
-        {   
-            image: '',
-            lastname: "Chiara",
-            firstname: "Charlotte - Ava",
-        }, 
-        {   
-            image: '',
-            lastname: "Chiara",
-            firstname: "Charlotte - Ava",
-        }, 
-        {   
-            image: '',
-            lastname: "Chiara",
-            firstname: "Charlotte - Ava",
-        }, 
-        {   
-            image: '',
-            lastname: "Chiara",
-            firstname: "Charlotte - Ava",
-        }, 
-        {   
-            image: '',
-            lastname: "Chiara",
-            firstname: "Charlotte - Ava",
-        }, 
-        {   
-            image: '',
-            lastname: "Chiara",
-            firstname: "Charlotte - Ava",
-        }, 
-    ];
+    const [eventInfo, setEventInfo] = useState({});
+    const [participantsList, setParticipantsList] = useState([]);
+    const [participantsRatings, setParticipantsRatings] = useState([]);
+    useEffect(() => {
+        Promise.all([
+          getEventById(param.eventID),
+          getParticipantsByEventId(param.eventID)
+        ])
+          .then(function ([eventResponse, participantsResponse]) {
+            setEventInfo(eventResponse.data);
+            setParticipantsList(participantsResponse.data);
+
+            const ratings = participantsResponse.data.map(participant => ({
+                userProfileID: participant.userProfileID,
+                value: 0
+            }));
+            setParticipantsRatings(ratings);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }, [param.eventID]);
+
+    const formattedStartTime = eventInfo.startTime ? 
+        format(new Date(eventInfo.startTime), "dd.MM.yyyy 'at' HH:mm")
+    : '';
+    const formattedEndTime = eventInfo.endTime ? 
+        format(new Date(eventInfo.endTime), "dd.MM.yyyy 'at' HH:mm")
+    : '';
+
+    const handleRatingChange = (index, newValue) => {
+        setParticipantsRatings(prevRatings => {
+          const updatedRatings = [...prevRatings];
+          updatedRatings[index].value = newValue;
+          return updatedRatings;
+        });
+    };
+
+    const sendRatings = () => {
+        participantsRatings.forEach(({ userProfileID, value }) => {
+            addRating({
+                userProfileID: userProfileID,
+                value: value
+            })
+            .then(response => {
+                console.log(response.status);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        });
+    }
     
     return(
         <>
@@ -52,30 +72,46 @@ const FeedbackForm = () => {
             <Paper elevation={0} className="paper">
                 <Grid container pb={2}>
                     <Grid item xs={12} py={2} className="grid">
-                        <Typography variant="h5">Feedback Form for Event</Typography>
+                        <Typography variant="h5">Feedback Form</Typography>
                     </Grid>
                     <Grid item xs={12} className="grid">
-                        <Typography variant="h6">'Taste - Tastic' I can cook</Typography>
+                        <Typography variant="h6">"{eventInfo.name}" Event</Typography>
                     </Grid>
                     <Grid item xs={12} className="grid">
-                        <CalendarMonthIcon className="icon"/><Typography variant="h6" px={1}>28 Oct. 2023</Typography>
+                        <EventAvailableIcon className="icon"/><Typography variant="h6" px={1}>Started {formattedStartTime}</Typography>
                     </Grid>
                     <Grid item xs={12} className="grid">
-                        <AccessTimeIcon className="icon"/><Typography variant="h6" px={1}>18:00</Typography>
+                        <EventBusyIcon className="icon"/><Typography variant="h6" px={1}>Ended {formattedEndTime}</Typography>
                     </Grid> 
                 </Grid>
                 <div className="container">
-                    {participants.map((participant, index) => {
+                    { participantsList.map((participant, index) => {
                         return(
                             <Grid container py={2} alignItems={"center"} key={index}>
                                 <Grid item xs={2} className="grid">
-                                    <Avatar alt="avatar" src={participant.image} className="img">{participant.lastname.charAt(0)}{participant.firstname.charAt(0)}</Avatar>
+                                    { participant.basicInfo.profilePicture !== "" ? (
+                                        <Avatar alt="avatar" src={participant.basicInfo.profilePicture} className="img"/>
+                                    )
+                                    :
+                                    (
+                                        <Avatar alt="avatar" sx={{ bgcolor: deepOrange[500], "&:hover": { bgcolor: deepOrange[900] }}} className="img">
+                                            <Typography variant="h1" component="span" sx={{ fontSize: "2rem" }}>
+                                                {participant.basicInfo.lastName.charAt(0)}
+                                                {participant.basicInfo.firstName.charAt(0)}
+                                            </Typography>
+                                        </Avatar>
+                                    )}
                                 </Grid>
                                 <Grid item xs={6} pr={4} className="grid">
-                                    <Typography variant="h6">{participant.lastname} {participant.firstname}</Typography>
+                                    <Typography variant="h6">{participant.basicInfo.lastName} {participant.basicInfo.firstName}</Typography>
                                 </Grid>
                                 <Grid item xs={4} className="grid" pr={4}>
-                                    <Rating sx={{fontSize: '2rem'}} precision={0.5}/>
+                                    <Rating
+                                        sx={{fontSize: '2rem'}}
+                                        precision={0.5}
+                                        value={participantsRatings[index].rating}
+                                        onChange={(event, newValue) => handleRatingChange(index, newValue)}
+                                    />
                                 </Grid>
                             </Grid>
                         )
@@ -83,7 +119,7 @@ const FeedbackForm = () => {
                 </div>
                 <Grid item xs={12} py={1}>
                     <Box display="flex" justifyContent="flex-end">
-                        <Button variant="contained">SEND</Button>
+                        <Button variant="contained" onClick={() => {sendRatings()}}>SEND</Button>
                     </Box>
                 </Grid>
             </Paper>
